7a49ddd4cbfc063c8dc4b426a11ab338fa8e9a41
exsystemCTL <exsystemctl@gmail.com>
2017-01-07 16:49:38 -0600

    Working IRC Loop

---
+++ sysBOT/sysBOT/Connection.cs
@@ -0,0 +1,36 @@
+﻿using TwitchLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.Collections.Specialized;
+using TwitchLib.Events.Client;
+
+namespace sysBOT
+{
+    public class Connection
+    {
+        static TwitchClient client;
+        static Connection()
+        {
+            client = new TwitchLib.TwitchClient(new TwitchLib.Models.Client.ConnectionCredentials(ConfigurationManager.AppSettings.Get("botUsername"), ConfigurationManager.AppSettings.Get("botOauth")), ConfigurationManager.AppSettings.Get("channel"), '!', '!', true);
+
+            client.OnConnected += clientConnected;
+            client.OnJoinedChannel += clientJoinedChannel;
+
+            client.Connect();
+        }
+
+        private static void clientJoinedChannel(object sender, OnJoinedChannelArgs e)
+        {
+            Console.WriteLine($"Client joined channel: {e.Channel}");
+        }
+
+        private static void clientConnected(object sender, OnConnectedArgs e)
+        {
+            Console.WriteLine("Client connected to Twitch!");
+        }
+    }
+}
